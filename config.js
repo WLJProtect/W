daf899104a48771a17043d85399f58511e607128
WLJProtect <99254818+WLJProtect@users.noreply.github.com>
2022-02-08 22:16:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ var config = {
         "心爱的小可爱",  // 同上...
         "今天是你的生日",
         "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
+        "第一个生日了哦",
+        "去年的这个时候差不多少一个月",
+        "我们在一起了",
         "今年要吃好的喽哦",
         "要把我家可爱猪猪喂饱饱",
         "然后抱走",
@@ -25,18 +25,18 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "心爱的小可爱": ",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        turn_on: "点开始啦",
+        play: "来点音乐？",
+        bannar_coming: "来点颜色？",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        story: "我爱你小雨",
     }
 };
